1f837bdc31a887f332949e2a6c1979014133eaec
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-01 18:23:22 +0530

    main.cpp

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int score;
+  score = 110;
+
+  const int uid = 12334;
+
+  int Arjun = 500;
+  Arjun = 1000;
+
+  // uid = 1223
+  cout << "Welcome to the cpp journy" << endl;
+  cout << "Welcome to the cpp1 journy" << endl;
+
+return 0;
+
+}

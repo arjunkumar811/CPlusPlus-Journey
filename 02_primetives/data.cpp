45832e3c9e7fe392b0ba038bfd19a4c566c01181
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-03 13:30:07 +0530

    data type

---
+++ 02_primetives/data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+int teaLeves = 50;
+float waterTem = 85.58888;
+double priceOfTea = 299.99;
+char teaGrade = 'A';
+bool isTeaReady = false;
+
+cout << waterTem << endl;
+cout << teaGrade << endl;
+cout << teaLeves << endl;
+cout << priceOfTea << endl;
+cout << isTeaReady << endl;
+
+return 0;
+
+}

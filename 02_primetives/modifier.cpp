1453dd041d0f0d54f4fa94352433ce24a9924830
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-03 13:37:05 +0530

    modifier data type

---
+++ 02_primetives/modifier.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+
+unsigned smallTeaPack = 1200; // only non-negettive integer 
+
+long long largeeaStronge = 100000000;
+
+short teaSample = 25;
+
+cout << largeeaStronge << endl;
+cout << smallTeaPack << endl;
+
+    return 0;
+}

16b646385de3625a58b4ea5005bb357b2ca721d2
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-03 14:03:30 +0530

    myString

--- 02_primetives/myString.cpp
+++ 02_primetives/myString.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
 
-using namespace sttd;
 
-int mian() {
+using namespace std;
+
+int main() {
+
+    string favoriteTea = "Lemon Tea \t";
+    string description = "Know as \"best\" tea"; // printt as it is 
+
+    cout << favoriteTea << endl; // Lemon Tea
+    cout << description << endl; // Know as "best" tea
 
     return 0;
 }

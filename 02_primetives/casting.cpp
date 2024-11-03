08913a4023e08682023ddae55a07c2bc9d920a68
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-03 13:50:56 +0530

    casting

--- 02_primetives/casting.cpp
+++ 02_primetives/casting.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
-int mian() {
+int main(){
 
+    float teaPrice = 49.99;
+    int roundedTeaPrice = (int) teaPrice;
 
+    int teaQuantity = 2;
+    int totalPrice = teaPrice * teaQuantity;
+
+    cout << totalPrice << endl; // 99
+    cout << roundedTeaPrice << endl; // 49
 
     return 0;
 }

98ea82229664d2bd488052472d4d997ce42c8c2c
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-10 01:29:50 +0530

    setup

--- 01_varconst/Assignments/Challenge 1.cpp
+++ 01_varconst/Assignments/Challenge 1.cpp
@@ -1,2 +1,12 @@
 // Tea Information Display Write a program that declares variables to store the type of tea, its price per kilogram (float), and its rating (char). Use data types effectively and print them in a formatted output using escape sequences.
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}

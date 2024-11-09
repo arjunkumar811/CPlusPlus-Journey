b2614cb45b37e06d950994e899ee21dd979bedc5
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-10 01:28:40 +0530

    Challenge 1.cpp

---
+++ 01_varconst/Assignments/Challenge 1.cpp
@@ -0,0 +1,2 @@
+// Tea Information Display Write a program that declares variables to store the type of tea, its price per kilogram (float), and its rating (char). Use data types effectively and print them in a formatted output using escape sequences.
+

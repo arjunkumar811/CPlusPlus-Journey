93fe31425291c92a470e92340df072f69e31833d
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-12 02:47:31 +0530

    Added Basic Structure

---
+++ 01_varconst/Assignments/Challenge 2.cpp
@@ -0,0 +1,16 @@
+/* Challenge 2:
+
+Modify Tea Prices Create a program where the user inputs a base price for tea. Use type casting to increase the price by 10% and display the rounded new price using explicit casting.
+> Hint: Use both float and int types, and demonstrate type casting. */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
+

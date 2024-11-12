8a3d23ad8700ef7aa40b2e9805338a78b1cd4b53
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-13 03:04:27 +0530

    Basic Structure

---
+++ 01_varconst/Assignments/Challenge 3.cpp
@@ -0,0 +1,15 @@
+## Challenge 3:
+
+Favorite Tea Input Write a program that takes the user’s favorite tea as input using getline and also asks how many cups of tea they want using cin. Display the result in a fun message.
+
+> Hint: Combine cin and getline carefully to avoid input issues.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}

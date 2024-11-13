df3b1645536c69b97ca5c920f7ff727586a2a788
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-13 18:57:03 +0530

    Added Challenge 1.cpp

--- 03_operaters/assignments/Challenge 1.cpp
+++ 03_operaters/assignments/Challenge 1.cpp
@@ -5,10 +5,26 @@
 
    #include <iostream> 
 
-   using namespace std;
+using namespace std;
+#include <string> 
 
-   int main() {
+int main() {
+    int packs;
+    double packPrice = 100;  // Price per pack
+    double totalPrice, finalPrice;
 
+    // Ask user to enter the number of packs
+    cout << "Enter the number of tea packs you want: ";
+    cin >> packs;
+
+    // Calculate total price
+    totalPrice = packs * packPrice;
+
+    // Apply a 10% tax
+    finalPrice = totalPrice * 1.10;
+
+    // Display the final price after tax
+    cout << "The total price after 10% tax is: " << finalPrice << endl;
 
     return 0;
-   }
+}

ae56e096037e21cbf6e804417bdec312f66eedc4
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-03 23:44:48 +0530

    2D arrays

---
+++ 08_array/four.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int chaiSales[3][7] = {
+    {10, 23, 34, 34, 34, 56, 45},
+    {10, 23, 34, 34, 34, 56, 45},
+    {10, 23, 34, 34, 34, 56, 45}
+};
+
+for(int i = 0; i < 3; i++){
+cout << "Iam at shop:" << i+1 << "\n";
+for(int j = 0; j < 7; j++) {
+    cout << chaiSales[i] [j] << "cups";
+}
+cout << "\n";
+}
+    return 0;
+}
